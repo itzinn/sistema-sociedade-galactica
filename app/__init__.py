e65f33af2a63231e21c2c27ab2bbef0d2ce148fe
Pablo Montemor <pablo.montemor@usp.br>
2024-06-05 20:32:18 -0300

    refactor: muda estrutura de arquivos da aplicação

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,30 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '[hashgrandona]'
 
-import app.routes
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    #logica de autenticação aqui
+    
+    return render_template('login.html')
+
+@app.route('/overview')
+def overview():
+    
+    #lógica para obter infos de overview aqui
+
+    return render_template('overview.html')
+
+@app.route('/relatorios')
+def relatorios():
+
+    #lógica para gerar relatorios aqui
+
+    return render_template('relatorios.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
