a04e9b00bd1b1446ef5e6bbe3efa12a036779494
Angela <106828615+AngelaMunante@users.noreply.github.com>
2024-06-19 10:25:55 -0300

    Update lider.sql

--- Coleções/lider.sql
+++ Coleções/lider.sql
@@ -5,7 +5,7 @@
 
 --Este arquivo contem o pacote lider com todos seus procedimentos e relatirios. Possui tambem as coleções usadas e triggers usado pelos pacotes.
 
---Coleções
+--Coleções usado  no procudere alterar_nome_faccao
 CREATE TYPE nf_row_type AS OBJECT (
 	nacao VARCHAR2(15),
 	faccao VARCHAR2(15)
@@ -22,9 +22,11 @@ CREATE OR REPLACE TYPE nf_table_type AS TABLE OF nf_row_type;
 CREATE OR REPLACE TYPE participa_table_type AS TABLE OF participa_row_type;
 /
 
+	
 --View e triggers
 
---View para gerenciamento de
+--View para gerenciamento de comunidade
+-- A visão GERENCIAMENTO_FACCAO da uma visão consolidada das informações sobre facções, nações, planetas e comunidades. Facilita o gerenciamento e a análise desses dados. Ela mostra quais facções dominam quais planetas, quais nações estão associadas a essas facções e quais comunidades estão presentes nesses planetas. A visão também verifica se uma comunidade está credenciada com uma facção específica, indicando isso com um status de 'CREDENCIADA' ou 'NAO CREDENCIADA'. Este status de credenciamento é determinado pela presença ou ausência de registros na tabela PARTICIPA.
 CREATE OR REPLACE VIEW GERENCIAMENTO_FACCAO AS
 SELECT 
 	F.LIDER,
@@ -45,6 +47,8 @@ FROM
 	COMUNIDADE C ON H.ESPECIE = C.ESPECIE AND H.COMUNIDADE = C.NOME LEFT JOIN 
 	PARTICIPA PA ON C.ESPECIE = PA.ESPECIE AND C.NOME = PA.COMUNIDADE AND NF.FACCAO = PA.FACCAO;
 
+
+--Trigger é utilizado para manter automaticamente atualizada a quantidade de planetas dominados por uma nação na tabela NACAO. Quando um novo planeta é dominado (inserido) ou deixa de ser dominado (atualizado ou deletado) na tabela DOMINANCIA, o trigger pega o nome da nação e quando a operação acaba ele recalcula a quantidade de planetas atualmente dominados por essa nação e atualiza a tabela NACAO com o valor correto. Isso garante que a tabela NACAO sempre tenha o número de planetas sob domínio de cada nação atualizado.
 
 CREATE OR REPLACE TRIGGER GERENCIAR_PARTICIPA
 	INSTEAD OF INSERT OR DELETE ON GERENCIAMENTO_FACCAO
@@ -72,7 +76,7 @@ BEGIN
 END;
 /
 
-
+--Trigger para fazer update de planetas na tabela Nacao quando um planeta novo é dominado ou deixa de ser dominado
 CREATE OR replace TRIGGER trg_update_qtd_planetas
 FOR INSERT OR UPDATE OR DELETE ON DOMINANCIA
 COMPOUND TRIGGER
@@ -118,6 +122,7 @@ COMPOUND TRIGGER
 END trg_update_qtd_planetas;
 
 
+-- Este trigger é usado para atualizar automaticamente a quantidade de nações associadas a uma facção na tabela FACCAO toda vez que uma entrada é inserida, atualizada ou deletada na tabela NACAO_FACCAO. Quando uma operação de inserção, atualização ou exclusão ocorre, o trigger pega o nome da facção e recalcula a quantidade de nações associadas à facção e atualiza a tabela FACCAO. 
 create or replace TRIGGER trg_update_qtd_nacoes
 FOR INSERT OR UPDATE OR DELETE ON NACAO_FACCAO
 COMPOUND TRIGGER
@@ -180,6 +185,7 @@ CREATE OR REPLACE PACKAGE PacoteLiderFaccao AS
 END PacoteLiderFaccao;
 /
 
+	
 CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 
 	--Função para saber a facção do lider
@@ -196,21 +202,25 @@ CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 	END faccao_de_lider;
 
 
+	-- O procedimento alterar_nome_faccao altera o nome de uma facção liderada pelo líder especificado através de seu CPI. Primeiro verifica se o usuário é de fato líder de alguma facção, caso contrário, lança uma exceção e exibe uma mensagem de erro. Depois coleta todas as associações atuais da facção nas tabelas NACAO_FACCAO e PARTICIPA, removendo-as temporariamente para evitar o erro de entradas filhas ao atualizar o nome da faccao. O nome da facção é então atualizado na tabela FACCAO, e as associações coletadas anteriormente são reatualizadas com o novo nome da facção. Por ultimo, o procedimento verifica se a operação foi bem-sucedida e confirma a alteração, ou, em caso de erro, exibe uma mensagem de erro e executa um rollback para garantir a integridade dos dados. 
 	PROCEDURE alterar_nome_faccao(p_cpi IN CHAR, p_novo_nome IN VARCHAR2) IS
 		v_nome_faccao FACCAO.NOME%TYPE;
 		v_nf_linhas nf_table_type;
 		v_participa_linhas participa_table_type;
+		nao_lider EXCEPTION;
+		erro_alterar EXCEPTION;
 
 	BEGIN
 		v_nome_faccao := faccao_de_lider(p_cpi);
 
 		IF v_nome_faccao IS NULL THEN
-			DBMS_OUTPUT.PUT_LINE('Usuário não é líder de nenhuma facção.');
-			RETURN;
+			RAISE nao_lider;
+			--RETURN;
 		END IF;
-		DBMS_OUTPUT.PUT_LINE('1');
+
+		--DBMS_OUTPUT.PUT_LINE('1');
 		SELECT CAST(COLLECT(nf_row_type(NACAO, FACCAO)) AS nf_table_type) INTO v_nf_linhas FROM NACAO_FACCAO WHERE FACCAO = v_nome_faccao;
-		DBMS_OUTPUT.PUT_LINE('2');
+		--DBMS_OUTPUT.PUT_LINE('2');
 		SELECT CAST(COLLECT(participa_row_type(FACCAO, ESPECIE, COMUNIDADE)) AS participa_table_type) INTO v_participa_linhas FROM PARTICIPA WHERE FACCAO = v_nome_faccao;
 
 		DELETE FROM NACAO_FACCAO WHERE FACCAO = v_nome_faccao;
@@ -237,7 +247,8 @@ CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 
 
 		IF SQL%ROWCOUNT = 0 THEN
-			DBMS_OUTPUT.PUT_LINE('Erro ao alterar o nome da facção.');
+			--DBMS_OUTPUT.PUT_LINE('Erro ao alterar o nome da facção.');
+			RAISE erro_alterar;
 		ELSE
 			DBMS_OUTPUT.PUT_LINE('Nome da facção alterado com sucesso de ' || v_nome_faccao || ' para ' || p_novo_nome);
 			COMMIT;
@@ -245,25 +256,31 @@ CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 
 		
 	EXCEPTION
+		WHEN erro_alterar THEN
+			DBMS_OUTPUT.PUT_LINE('Erro ao alterar o nome da facção.');
+		WHEN nao_lider THEN
+			DBMS_OUTPUT.PUT_LINE('Usuário não é líder de nenhuma facção.');
 		WHEN OTHERS THEN
 			DBMS_OUTPUT.PUT_LINE('Erro ao alterar o nome da facção: ' || SQLERRM);
 			ROLLBACK;
 	END;
 
 
+	-- O procedimento cadastrar_nova_comunidade associa uma nova comunidade à facção liderada pelo líder. Primeiramente, verifica se o usuário é de fato líder de alguma facção, caso contrário, exibe uma mensagem de erro e encerra a execução. Em seguida, verifica se a comunidade já está associada à facção na view GERENCIAMENTO_FACCAO para evitar duplicações. Se a comunidade não estiver associada, o procedimento recupera a espécie da comunidade e insere a nova associação na tabela GERENCIAMENTO_FACCAO. Caso a operação seja bem-sucedida, uma mensagem de confirmação é exibida e a transação é confirmada com um COMMIT.
 	PROCEDURE cadastrar_nova_comunidade(p_cpi IN CHAR, p_comunidade IN VARCHAR2) IS
 		v_nome_facao FACCAO.NOME%TYPE;
 		v_especie ESPECIE.NOME%TYPE;
 		v_check_faccao FACCAO.NOME%TYPE;
 
 		faccao_errado EXCEPTION;
+		erro_alterar EXCEPTION;
 
 	BEGIN
 		v_nome_facao := faccao_de_lider(p_cpi);
 
 		IF v_nome_facao IS NULL THEN
-			DBMS_OUTPUT.PUT_LINE('Usuário não é líder de nenhuma facção.');
-			RETURN;
+			RAISE erro_alterar;
+			--RETURN;
 		END IF;
 
 		SELECT FACCAO INTO v_check_faccao FROM GERENCIAMENTO_FACCAO WHERE COMUNIDADE = p_comunidade AND FACCAO = v_nome_facao;
@@ -278,6 +295,8 @@ CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 		COMMIT;
 
 	EXCEPTION
+		WHEN erro_alterar THEN
+			DBMS_OUTPUT.PUT_LINE('Usuário não é líder de nenhuma facção.');
 		WHEN NO_DATA_FOUND THEN
 			DBMS_OUTPUT.PUT_LINE('A comunidade não pertence a faccao.');
 		WHEN OTHERS THEN
@@ -286,6 +305,7 @@ CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 	END;
 
 
+	-- O procedimento eliminar_comunidade remove a associação de uma comunidade específica da facção liderada pelo líder. Primeiramente, ele verifica se o usuário é de fato líder de alguma facção, exibindo uma mensagem de erro e encerrando a execução caso contrário. Em seguida, verifica se a comunidade está associada à facção na visão GERENCIAMENTO_FACCAO. Se a comunidade estiver associada, o procedimento remove a associação da tabela GERENCIAMENTO_FACCAO e confirma a remoção exibindo uma mensagem de confirmação e executando um COMMIT. Caso a comunidade não seja encontrada ou ocorra um erro durante o processo, uma mensagem de erro é exibida e a transação é revertida com um ROLLBACK para garantir a integridade dos dados. 
 	PROCEDURE eliminar_comunidade(p_cpi IN CHAR, p_comunidade IN VARCHAR2) IS
 		v_nome_facao FACCAO.NOME%TYPE;
 		v_check_faccao FACCAO.NOME%TYPE;
@@ -314,6 +334,7 @@ CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 	END;
 
 
+	--O procedimento indicar_novo_lider tem como objetivo atualizar o líder de uma facção, nomeando um novo líder baseado no CPI fornecido. Primeiramente, ele verifica se o usuário atual é realmente líder de uma facção, exibindo uma mensagem de erro e encerrando a execução caso contrário. Em seguida, obtém a nação do novo líder candidato e verifica se este líder pertence à mesma facção da nação associada, garantindo que a mudança seja válida. Se todas as condições forem satisfeitas, o procedimento atualiza a tabela FACCAO com o novo líder e confirma a operação com um COMMIT, exibindo uma mensagem de sucesso. Caso o novo líder não pertença à facção ou ocorra qualquer outro erro durante o processo, uma mensagem de erro é exibida e a transação é revertida com um ROLLBACK, 
 	PROCEDURE indicar_novo_lider(p_cpi IN CHAR, p_novo_lider IN CHAR) IS
 		v_nacao LIDER.NACAO%TYPE;
 		v_nome_facao FACCAO.NOME%TYPE;
@@ -350,7 +371,8 @@ CREATE OR REPLACE PACKAGE BODY PacoteLiderFaccao AS
 			ROLLBACK;
 	END;
 
---ver se foi atualizado o numero de nações apos delete - FEITO COM TRIGGER
+
+	-- O procedimento remover_relacao_facao tem como objetivo remover a relação entre uma facção e uma nação especificada. Primeiro, verifica se o usuário é realmente líder de alguma facção e se a nação fornecida não é nula, lançando uma exceção se qualquer uma dessas condições não for satisfeita. Em seguida, tenta deletar a entrada correspondente na tabela NACAO_FACCAO que associa a facção e a nação. Após a remoção, verifica se alguma linha foi afetada; caso contrário, lança uma exceção indicando que nada foi deletado. Se a remoção for bem-sucedida, exibe uma mensagem de confirmação e executa um COMMIT para confirmar a transação. Em caso de erro, uma mensagem apropriada é exibida e a transação é revertida com um ROLLBACK. Quando é deletado uma entrada da tabela nação facção o trigger atualiza a quantidade de planetas
 	PROCEDURE remover_relacao_facao(cpi IN CHAR, nacao IN VARCHAR2) IS
 		v_nome_facao FACCAO.NOME%TYPE;
 		v_nacao NACAO_FACCAO.NACAO%TYPE;
@@ -465,31 +487,32 @@ END PacoteLiderFaccao;
 
 
 
-DEClARE
-  nome_nacao NACAO.nome%TYPE;
-  nome_lider LIDER.cpi%TYPE;
-  novo_nome_faccao FACCAO.nome%TYPE;
-  comunidade_cadastrar COMUNIDADE.NOME%TYPE;
-  novo_lider NACAO.nome%TYPE;
-  lider_com LIDER.cpi%TYPE;
+--Exemplo de uso em sql ELLIMINAR DEPOIS!!!
+--DEClARE
+--  nome_nacao NACAO.nome%TYPE;
+--  nome_lider LIDER.cpi%TYPE;
+--  novo_nome_faccao FACCAO.nome%TYPE;
+--  comunidade_cadastrar COMUNIDADE.NOME%TYPE;
+--  novo_lider NACAO.nome%TYPE;
+--  lider_com LIDER.cpi%TYPE;
 
-BEGIN
-  nome_lider := '233.123.456-11'; --obtido pelo login do usuário no sistema
-  nome_nacao := 'NacaoP'; --Usuario que possui a função de lider insere o nome_nacao que deseja eliminar
-  novo_nome_faccao := 'amarillo'; --como vai ser conseguido? por meio da aplicação
-  comunidade_cadastrar := 'Comunidade A';
-  novo_lider := '111.456.789-00';
-  lider_com := '123.123.123-10';
+--BEGIN
+--  nome_lider := '233.123.456-11'; --obtido pelo login do usuário no sistema
+--  nome_nacao := 'NacaoP'; --Usuario que possui a função de lider insere o nome_nacao que deseja eliminar
+--  novo_nome_faccao := 'amarillo'; --como vai ser conseguido? por meio da aplicação
+--  comunidade_cadastrar := 'Comunidade A';
+--  novo_lider := '111.456.789-00';
+--  lider_com := '123.123.123-10';
 
 
   --PacoteLiderFaccao.alterar_nome_faccao(nome_lider, novo_nome_faccao);
   --PacoteLiderFaccao.indicar_novo_lider(nome_lider, novo_lider);
   --PacoteLiderFaccao.cadastrar_nova_comunidade(lider_com, comunidade_cadastrar);
   --PacoteLiderFaccao.eliminar_comunidade(lider_com, comunidade_cadastrar);
-  PacoteLiderFaccao.remover_relacao_facao(novo_lider, nome_nacao);
+ -- PacoteLiderFaccao.remover_relacao_facao(novo_lider, nome_nacao);
 
-END;
-/
+--END;
+--/
 
 
 
